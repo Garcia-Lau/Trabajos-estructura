d87f715bc94d9360dc9182c5540f74163fb03075
Laura Garcia <lmgarciau@ufpso.edu.co>
2026-02-27 08:06:49 -0500

    para revisar funcionamiento

--- lista-simples/sistema-contacto/src/Main.java
+++ lista-simples/sistema-contacto/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ListaContacto lista = new ListaContacto();
+
+        lista.insertarFinal(new contacto("ana", "3213241234", "ana@gmail.com"));
+        lista.insertarFinal(new contacto("ana", "3213241234", "ana@gmail.com"));
+        lista.insertarFinal(new contacto("ana", "3213241234", "ana@gmail.com"));
+        lista.insertarFinal(new contacto("ana", "3213241234", "ana@gmail.com"));
+
+        System.out.println("lista actual \n");
+        lista.lista();
+
+        System.out.println("\nBuscar ana :");
+        System.out.println(lista.buscar("ana"));
+
+        System.out.println("\neliminar andres :");
+        System.out.println(lista.eliminar("andres"));
+
+        System.out.println("\n lista actual :");
+
+      lista.lista();
     }
 }
--- lista-simples/sistema-contacto/src/contacto.java
+++ lista-simples/sistema-contacto/src/contacto.java
@@ -9,6 +9,30 @@ public class contacto {
         this.nombre = nombre;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
     @Override
     public String toString() {
         return "contacto{" +
